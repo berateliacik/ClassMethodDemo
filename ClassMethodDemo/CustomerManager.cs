171974c0d62b15e0292177ac2550fe93553e641d
Berat <Berat@DESKTOP-CEI400U>
2022-02-03 23:25:35 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer Customer)
+        {
+
+            Console.WriteLine("Customer Name: "+ Customer.Name+" Customer Surname: "+Customer.Surname);
+            Console.WriteLine("Customer Added.");
+
+        }
+
+        public void CustomerDelete(Customer Customer)
+        {
+            Console.WriteLine("Customer Name: " + Customer.Name + " Customer Surname: " + Customer.Surname);
+            Console.WriteLine("Customer Deleted.");
+
+        }
+
+        public void CustomerShow(Customer[] customer)
+        {
+
+            foreach (var customers in customer)
+            {
+                Console.WriteLine("Customer Name: " + customers.Name + " Customer Surname: " + customers.Surname);
+            }
+
+            Console.WriteLine("Customers have been shown.");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer Customer1 = new Customer();
+
+            Customer1.Id = 01;
+            Customer1.Name="Ali";
+            Customer1.Surname = "Alioğlu";
+            Customer1.Adress = "11111. sk. no 1 kat 1 ";
+            Customer1.PhoneNumber = 05000000;
+
+
+            Customer Customer2 = new Customer();
+            Customer2.Id = 02;
+            Customer2.Name = "Veli";
+            Customer2.Surname = "Velioğlu";
+            Customer2.Adress = "22222. sk. no 2 kat 2 ";
+            Customer2.PhoneNumber = 051111111;
+
+
+            Customer[] customers = new Customer[] { Customer1, Customer2 };
+
+            CustomerManager customer = new CustomerManager();
+            customer.CustomerAdd(Customer1);
+            customer.CustomerDelete(Customer2);
+            customer.CustomerShow(customers);
+
+
+
+
+
+
+        }
+    }
+}
